03372c2ff1f17afb524df799ecde298f9e54e7e4
Katze <googletan@mail.ru>
2024-11-25 15:52:21 +0300

    separate InitialBasedState

---
+++ widget/src/main/scala/library/InitialBasedState.scala
@@ -0,0 +1,46 @@
+package me.katze.gui4s.widget
+package library
+
+import stateful.*
+
+import cats.*
+import cats.syntax.all.{*, given}
+
+final case class InitialBasedState[
+  Widget,
+  Update[+_, +_],
+  Place[+_],
+  Dealloc,
+  T: Equiv,
+  ParentEvent, ChildEvent,
+  WidgetTask[+_]
+](
+    initialState: T,
+    currentState: T,
+    eventHandler  : (T, ChildEvent) => EventReaction[WidgetTask[ChildEvent], T, ParentEvent],
+    renderState   : T => Place[Widget],
+    typeCheck     : RichTypeChecker[(T, T)],
+    deallocState : T => Dealloc,
+    liftEventReaction : LiftEventReaction[Update, WidgetTask[Any]]
+) extends State[[U] =>> Update[U, ParentEvent], Dealloc, ChildEvent, Place[Widget]]:
+  override def handleEvent(event: ChildEvent): Update[State[[U] =>> Update[U, ParentEvent], Dealloc, ChildEvent, Place[Widget]], ParentEvent] =
+    liftEventReaction.lift(
+      eventHandler(currentState, event).mapState(newState => copy(currentState = newState))
+    )
+  end handleEvent
+
+  override def dealloc: Dealloc = deallocState(this.currentState)
+
+  override def render: Place[Widget] = renderState(currentState)
+
+  override def state: Any = (initialState, currentState)
+
+  override def mergeWithOldState(maybeOldState: Any): State[[U] =>> Update[U, ParentEvent], Dealloc, ChildEvent, Place[Widget]] =
+    val (oldInitialState, oldState) = typeCheck.tryCast(maybeOldState).valueOr(error => throw Exception(error))
+    if Equiv[T].equiv(oldInitialState, initialState) then
+      copy(oldInitialState, currentState) // TODO Надо проверить, точно ли куррент. Кажется, будто надо брать старое, если оное есть.
+    else
+      this
+    end if
+  end mergeWithOldState
+end InitialBasedState
--- widget/src/main/scala/library/StatefulLibrary.scala
+++ widget/src/main/scala/library/StatefulLibrary.scala
@@ -20,37 +20,27 @@ def statefulWidget[
 )(
     name          : String,
     initialState  : T,
-    dealloc_ : T => Recomposition,
+    deallocState : T => Recomposition,
     eventHandler  : (T, ChildEvent) => EventReaction[WidgetTask[ChildEvent], T, ParentEvent],
     renderState   : T => Place[Widget[Update, Draw, Place, Recomposition, ChildEvent, DownEvent]],
     typeCheck     : RichTypeChecker[(T, T)]
 ): Place[Widget[Update, Draw, Place, Recomposition, ParentEvent, DownEvent]] =
-  final case class StateImpl(
-                              initialState: T,
-                              currentState: T
-                            ) extends State[[U] =>> Update[U, ParentEvent], Recomposition, ChildEvent, Place[Widget[Update, Draw, Place, Recomposition, ChildEvent, DownEvent]]]:
-    override def handleEvent(event: ChildEvent): Update[State[[U] =>> Update[U, ParentEvent], Recomposition, ChildEvent, Place[Widget[Update, Draw, Place, Recomposition, ChildEvent, DownEvent]]], ParentEvent] =
-      liftEventReaction.lift(
-        eventHandler(currentState, event).mapState(StateImpl(initialState, _))
-      )
-    end handleEvent
+  statefulWidget(
+    name,
+    InitialBasedState(initialState, initialState, eventHandler, renderState, typeCheck, deallocState, liftEventReaction)
+  )
+end statefulWidget
 
-    override def dealloc: Recomposition = dealloc_(this.currentState)
-
-    override def render: Place[Widget[Update, Draw, Place, Recomposition, ChildEvent, DownEvent]] = renderState(currentState)
-
-    override def state: Any = (initialState, currentState)
-
-    override def mergeWithOldState(maybeOldState: Any): State[[U] =>> Update[U, ParentEvent], Recomposition, ChildEvent, Place[Widget[Update, Draw, Place, Recomposition, ChildEvent, DownEvent]]] =
-      val (oldInitialState, oldState) = typeCheck.tryCast(maybeOldState).valueOr(error => throw Exception(error))
-      if Equiv[T].equiv(oldInitialState, initialState) then
-        StateImpl(oldInitialState, currentState)
-      else
-        this
-      end if
-    end mergeWithOldState
-  end StateImpl
-
-  val state = StateImpl(initialState, initialState)
+def statefulWidget[
+  Update[+_, +_] : BiMonad : CatchEvents,
+  Draw : StatefulDraw,
+  Place[+_] : FlatMap,
+  Recomposition,
+  ParentEvent, ChildEvent,
+  DownEvent,
+](
+    name          : String,
+    state : State[[U] =>> Update[U, ParentEvent], Recomposition, ChildEvent, Place[Widget[Update, Draw, Place, Recomposition, ChildEvent, DownEvent]]]
+): Place[Widget[Update, Draw, Place, Recomposition, ParentEvent, DownEvent]] =
   state.render.map(Stateful(name, state, _))
 end statefulWidget
