1e860e10f65c2ae322c191d0939c2bc345420236
Katze <googletan@mail.ru>
2024-12-01 20:09:35 +0300

    text draw example

--- draw/src/main/scala/lwjgl/Example.scala
+++ draw/src/main/scala/lwjgl/Example.scala
@@ -83,24 +83,27 @@ object Example extends App:
   end init
 
   private def loop(): Unit =
-    // This line is critical for LWJGL's interoperation with GLFW's
-    // OpenGL context, or any context that is managed externally.
-    // LWJGL detects the context that is current in the current thread,
-    // creates the GLCapabilities instance and makes the OpenGL
-    // bindings available for use.
+  {
     GL.createCapabilities
-    // Set the clear color
-    glClearColor(1.0f, 1.0f, 0.0f, 0.0f)
-    // Run the rendering loop until the user has attempted to close
-    // the window or has pressed the ESCAPE key.
+    val vertices = Array(0.0f, 0.5f, 0.0f, -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f)
+    val vertexBuffer = memAllocFloat(vertices.length)
+    vertexBuffer.put(vertices).flip
     while (!glfwWindowShouldClose(window))
-      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) // clear the framebuffer
-
-      glfwSwapBuffers(window) // swap the color buffers
-
-      // Poll for window events. The key callback above will only be
-      // invoked during this call.
+    {
       glfwPollEvents()
-    end while
-  end loop
+      glClear(GL_COLOR_BUFFER_BIT)
+      
+      
+      
+      glColor3f(0.0f, 1.0f, 0.0f)
+      glEnableClientState(GL_VERTEX_ARRAY)
+      glVertexPointer(3, GL_FLOAT, 0, vertexBuffer)
+      glDrawArrays(GL_TRIANGLES, 0, 3)
+      glDisableClientState(GL_VERTEX_ARRAY)
+      glfwSwapBuffers(window)
+    }
+    memFree(vertexBuffer)
+    glfwDestroyWindow(window)
+    glfwTerminate()
+  }
 end Example
