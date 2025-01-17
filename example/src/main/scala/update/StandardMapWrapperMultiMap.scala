762c00e8211b9cb0d82b760001a3e91054d2a984
Katze <googletan@mail.ru>
2025-01-17 09:31:09 +0300

    rename StlWrapperMultiMap to StandardMapWrapperMultiMap

---
+++ example/src/main/scala/update/StandardMapWrapperMultiMap.scala
@@ -0,0 +1,35 @@
+package me.katze.gui4s.example
+package update
+
+final case class StandardMapWrapperMultiMap[K, V](realMap: Map[K, Set[V]]) extends MultiMap[K, V]:
+  override def add(key : K, value: V): MultiMap[K, V] =
+    StandardMapWrapperMultiMap(
+      realMap
+        .updated(
+          key, 
+          realMap.getOrElse(key, Set()).+(value)
+        )
+    )
+  end add
+
+  override def keys: Set[K] = realMap.keySet
+
+  override def values(key: K): Set[V] =
+    realMap.getOrElse(key, Set())
+  end values
+  override def remove(key: K, value: V): MultiMap[K, V] =
+    StandardMapWrapperMultiMap(
+      realMap
+        .updated(
+          key,
+          realMap.getOrElse(key, Set()).excl(value)
+        )
+    )
+  end remove
+  
+  override def removeAll(key: K): (MultiMap[K, V], Set[V]) =
+    val value = realMap.getOrElse(key, Set())
+    (StandardMapWrapperMultiMap(realMap.-(key)), value)
+  end removeAll
+end StandardMapWrapperMultiMap
+
