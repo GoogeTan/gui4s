5c796d03f691a93a9527e12ec77ab7a4af173d4f
Katze <googletan@mail.ru>
2026-01-16 08:26:23 +0300

    Remove unused shaper methods

--- desktop/skija/src/main/scala/gui4s/desktop/skija/shaper/Shaper.scala
+++ desktop/skija/src/main/scala/gui4s/desktop/skija/shaper/Shaper.scala
@@ -8,24 +8,3 @@ import io.github.humbleui.skija.{Font, TextBlob}
 def createShaper[F[_] : Sync as S]: Resource[F, Shaper] =
   Resource.fromAutoCloseable(S.delay(Shaper.make()))
 end createShaper
-
-def shape[F[_] : Sync as S](shaper : Shaper, text : String, font : Font) : F[TextBlob] =
-  S.delay(
-    shaper.shape(text, font)
-  )
-end shape
-
-def shape[F[_] : Sync as S](shaper : Shaper, text : String, font : Font, width : Float) : F[TextBlob] =
-  S.delay(
-    shaper.shape(text, font, width)
-  )
-end shape
-
-def shape[F[_] : Sync](shaper : Shaper, text : String, font : Font, maybeWidth : Option[Float]) : F[TextBlob] =
-  maybeWidth match
-    case Some(width) =>
-      shape(shaper, text, font, width)
-    case None =>
-      shape(shaper, text, font)
-  end match
-end shape
