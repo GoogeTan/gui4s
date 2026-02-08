5c81555ff8581a65fb236f99ee13a233ae2e41c4
Katze <googletan@mail.ru>
2026-02-08 12:29:23 +0300

    Add SkPaint Refactor skija a bit

---
+++ desktop/skija/src/main/scala/gui4s/desktop/skija/SkPaint.scala
@@ -0,0 +1,34 @@
+package gui4s.desktop.skija
+
+import io.github.humbleui.skija.*
+
+final case class SkPaint(
+  color: Int = 0xFF000000,
+  style: PaintMode = PaintMode.FILL,
+  isAntiAlias: Boolean = false,
+  isDither: Boolean = false,
+  stroke: StrokeOptions = StrokeOptions(),
+  shader: Option[Shader] = None,
+  pathEffect: Option[PathEffect] = None,
+  maskFilter: Option[MaskFilter] = None,
+  colorFilter: Option[ColorFilter] = None,
+  imageFilter: Option[ImageFilter] = None,
+  blendMode: BlendMode = BlendMode.SRC_OVER
+):
+  def toSkia : Paint =
+    val res = stroke.applyToSkia(
+      Paint()
+        .setColor(color)
+        .setMode(style)
+        //.setAntiAlias(isAntiAlias) this breaks skia shaders.
+        .setDither(isDither)
+    )
+    shader.foreach(res.setShader)
+    pathEffect.foreach(res.setPathEffect)
+    maskFilter.foreach(res.setMaskFilter)
+    colorFilter.foreach(res.setColorFilter)
+    imageFilter.foreach(res.setImageFilter)
+    res.setBlendMode(blendMode)
+    res
+  end toSkia
+end SkPaint
---
+++ desktop/skija/src/main/scala/gui4s/desktop/skija/StrokeOptions.scala
@@ -0,0 +1,15 @@
+package gui4s.desktop.skija
+
+import io.github.humbleui.skija.{PaintStrokeCap, PaintStrokeJoin}
+
+case class StrokeOptions(
+  width: Float = 0.0f, // 0.0f = "Hairline" (always 1px wide)
+  miterLimit: Float = 4.0f,
+  cap: PaintStrokeCap = PaintStrokeCap.BUTT,
+  join: PaintStrokeJoin = PaintStrokeJoin.MITER
+):
+  def applyToSkia(paint: Paint) = 
+    paint.setStrokeWidth(width)
+      .setStrokeMiter(miterLimit)
+      .setStrokeCap(cap)
+      .setStrokeJoin(join)
